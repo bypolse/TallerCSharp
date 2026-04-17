2e926ae54c3bfb99bb87b15d272e394475bc1c79
bypolse <juanusseche@gmail.com>
2026-04-17 11:29:58 -0400

    Agregando manipulacion de texto

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: Usuario
- * Fecha: 17/04/2026
- * Hora: 10:53
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
+﻿
 using System;
 
 namespace TallerIUJO01
@@ -14,9 +7,23 @@ namespace TallerIUJO01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("====Taller 01====");
 			
-			// TODO: Implement Functionality Here
+			//1. El dato del usuario
+			string registroUsuario = "    ID_777;  JuanUseche;  EVALUACION;   95";
+			
+			Console.WriteLine(registroUsuario);
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El ID es: {0} del usuario {1} de la materia {2} con la nota {3}",id,nombre,tarea,nota));
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
